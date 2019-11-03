24f29d05d37bd67953b1d6f8e1e714f39186455a
cledersonbc <cledersonbc@live.com>
2019-11-02 23:51:43 -0300

    update on achievements

--- js/app.js
+++ js/app.js
@@ -721,11 +721,11 @@ System.register("ts/data/AchievementData", [], function (exports_21, context_21)
                             description: '<p>Em 2013 e em 2014 fui <b>representante de sala</b> no ensino técnico. Também fui representante de sala na metade do curso na faculdade (2016-2017) e vice-representante na outra metade (2017-2018).</p>'
                         },
                         {
-                            title: 'Palestra na faculdade',
+                            title: 'Palestrante na faculdade',
                             description: '<p>Em 2017, na Semana Nacional de Ciência e Tecnologia (SNCT), fui <b>palestrante</b> na <b>Fatec-ZL</b> sobre <b>Escritos digitais e cibercultura – uma leitura das narrativas literária fílmica: Fahrenheit 451</b>. Também palestrei um pouco sobre distopia da ficção científica, citando obras reconhecidas do gênero.</p>'
                         },
                         {
-                            title: 'Monitoria voluntária na faculdade',
+                            title: 'Monitor voluntário na faculdade',
                             description: '<p>Em 2017, durante dois semestres desse mesmo ano, fui <b>monitor de cálculo</b> na <b>Fatec-ZL</b>. Ensinei e tirei dúvidas de alunos sobre funções, taxa de variação média, limites, derivadas e integrais.</p>'
                         },
                         {
@@ -734,7 +734,7 @@ System.register("ts/data/AchievementData", [], function (exports_21, context_21)
                         },
                         {
                             title: '1º Artigo publicado',
-                            description: '<p>Em 05 de dezembro de 2018 eu publiquei meu primeiro artigo acadêmico, intitulado "<b>Vigilantismo e Sociedade: controle e poder na era da internet</b>", na <b>EnGeTec</b>.</p>'
+                            description: '<p>Em 05 de dezembro de 2018 publiquei meu primeiro artigo acadêmico, intitulado "<b>Vigilantismo e Sociedade: controle e poder na era da internet</b>", na <b>EnGeTec</b>.</p>'
                         }
                     ];
                 }
